af1c022e40e920a34334717a3c1bd25a38523ed3
msalahuddin267 <msalahuddin267@gmail.com>
2024-12-13 21:39:01 +0600

    jwt auth implement

--- JWT with FastAPI/main.py
+++ JWT with FastAPI/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, status, Depends, HTTPException
 from typing import Annotated
 from sqlalchemy.orm import Session
-import models
+import models, auth
 from database import engine, SessionLocal
 
 app = FastAPI()
+app.include_router(auth.router)
 
 models.Base.metadata.create_all(bind=engine)
 
