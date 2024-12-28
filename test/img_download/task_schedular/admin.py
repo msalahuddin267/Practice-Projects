4f6f2e52b601644ae9a3a20cb2792af99a31e512
msalahuddin267 <msalahuddin267@gmail.com>
2024-12-28 11:22:35 +0600

    update projects

--- test/img_download/task_schedular/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CronJobControl
-
-@admin.register(CronJobControl)
-class CronJobControlAdmin(admin.ModelAdmin):
-    list_display = ('is_active', 'interval')
